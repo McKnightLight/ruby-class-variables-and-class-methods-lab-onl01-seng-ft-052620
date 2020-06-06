bf6c33f05f75af06c8000f31ce8def8e8cb2f652
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 23:28:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,20 @@ class Song
   
   attr_reader :name, :artist, :genre
   
+  @@count = 0 
+  @@artists = [] 
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
+    @@count += 1 
+    @@artists << artist 
+    @@genres << genre 
   end
+  
+  def self.count
+  end 
   
 end
