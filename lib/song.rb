0518ac220dc8db3f917ff895a071c5ce9036bd56
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 23:25:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,11 @@
 class Song 
   
+  attr_reader :name, :artist, :genre)
   
-end 
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+  end
+  
+end
