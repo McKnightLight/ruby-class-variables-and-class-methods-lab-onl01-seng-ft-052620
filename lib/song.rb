3dd240568607d408f87c61ed7b0a86fe58ea1a54
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 23:33:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ class Song
   def self.genre_count 
     # @@genres - non-unique array of genres
     # self.genres - unique array of genres 
-    
+       genre_hash = {}
     self.genres.each do |genre|
-      genre_hash = {}
+   
     
   end 
   
